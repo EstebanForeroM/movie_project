0b00365a26e0d356fed7791efc288073744fb6ba
juanestebanforeromontejo <juanesforeromontejo@gmail.com>
2024-09-25 21:53:40 -0500

    maked firs login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,26 @@
-
-
-import React from 'react'
-
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="flex relative my-10 w-[800px] h-96 border-2 border-pink-500
+        flex-col rounded-2xl p-5">
+
+        <h4 className="h4 mb-5">
+          Login to ... 
+        </h4>
+
+        <h5 className="h5 relative">UserName</h5>
+        <input type="text" className="border border-border rounded-xl bg-gray-950 relative py-2 px-2 mt-5 mb-5 outline-none
+          text-gray-400 placeholder:text-gray-900" placeholder="put your username"/>
+
+        <h5 className="h5 relative">password</h5>
+        <input type="text" className="border border-border rounded-xl bg-gray-950 relative py-2 px-2 mt-5 mb-5 outline-none 
+          text-gray-400 placeholder:text-gray-900" placeholder="put your password"/>
+
+        <button className="">
+          Submit
+        </button>
+      </div>
+    </div>
   )
 }
 
